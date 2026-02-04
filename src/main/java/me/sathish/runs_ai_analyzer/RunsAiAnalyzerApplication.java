d1486e602671197e0cdcad86cd2c12748ca52ef7
Sathish Jayapal <sjayapal@skminfotech.com>
2026-02-04 08:38:08 -0600

    feat: Configure Spring Boot project with core dependencies

---
+++ src/main/java/me/sathish/runs_ai_analyzer/RunsAiAnalyzerApplication.java
@@ -0,0 +1,12 @@
+package me.sathish.runs_ai_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunsAiAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunsAiAnalyzerApplication.class, args);
+    }
+}
